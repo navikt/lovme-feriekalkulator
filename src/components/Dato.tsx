ac1c0f8b5fa7e4c1bf10953f44ac067cba748d71
Stein Olav F <stein.olav.loset.frey@nav.no>
2023-06-26 14:53:52 +0200

    fix datepicker

--- src/components/Dato.tsx
+++ src/components/Dato.tsx
@@ -1,12 +1,14 @@
+import React, { ReactElement } from "react";
 import { DatePicker } from "@navikt/ds-react";
 
-const Dato = () => {
+const Dato = (): ReactElement => {
+
   return (
     <DatePicker.Standalone
       onSelect={console.log}
       dropdownCaption
-      fromDate={new Date("1 Oct 2020")}
-      toDate={new Date("1 Oct 2024")}
+      fromDate={new Date("Jan 1 2018")}
+      toDate={new Date("Jan 1 2025")}
     />
   );
 };
