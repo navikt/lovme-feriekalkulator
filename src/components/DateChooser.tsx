b3cacc876bb381a0d4919e01c07ee776007a1957
jonasnico <jonas.nicolaysen@navn.no>
2023-06-28 17:23:21 +0200

    remove unused imports/functions

--- src/components/DateChooser.tsx
+++ src/components/DateChooser.tsx
@@ -1,13 +1,13 @@
 import { DatePicker, useRangeDatepicker,  } from "@navikt/ds-react";
-import { format, subYears, startOfYear, addYears, endOfYear, getDayOfYear, getISODay, Interval, subDays, intervalToDuration } from 'date-fns';
+import { format, subYears, startOfYear, addYears, endOfYear } from 'date-fns';
 import { useEffect, useState } from "react";
 
 const DateChooser = () => {
 
   const [fromDate, setFromDate] = useState(new Date());
   const [toDate, setToDate] = useState(new Date());
+  // TODO: Display disse til bruker
   const [diffDays, setDiffDays] = useState<Number>(0);
-  const [newDate, setNewDate] = useState(new Date());
 
   const fiveYearsAgo = () => {
     const today = new Date();
@@ -31,7 +31,6 @@ const DateChooser = () => {
     return lastOfDecember;
   }
 
-  // TODO: Håndtere skuddår?
   const daysBetweenDates = (from: Date, to:Date) : Number => {
 
     var diff = Math.abs(from.getTime() - to.getTime());
@@ -56,43 +55,10 @@ const DateChooser = () => {
         */
         if (selectedRange?.from !== undefined && selectedRange?.to !== undefined) {
           setDiffDays(daysBetweenDates(selectedRange.from, selectedRange.to));
+          console.log(diffDays);
         }
       }, 
     });
-
-    /* FJERNES SENERE
-    function dateDiff(startingDate: Date, endingDate: Date) {
-      
-      if ((!endingDate || !startingDate) || startingDate > endingDate) {
-        return -1;
-      }
-      
-      let startDate = new Date(new Date(startingDate).toISOString().substr(0, 10));
-      let endDate = new Date(new Date(endingDate).toISOString().substr(0, 10));
-      const startYear = startDate.getFullYear();
-      const february = (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0 ? 29 : 28;
-      const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    
-      let yearDiff = endDate.getFullYear() - startYear;
-      let monthDiff = endDate.getMonth() - startDate.getMonth();
-      if (monthDiff < 0) {
-        yearDiff--;
-        monthDiff += 12;
-      }
-      let dayDiff = endDate.getDate() - startDate.getDate();
-      if (dayDiff < 0) {
-        if (monthDiff > 0) {
-          monthDiff--;
-        } else {
-          yearDiff--;
-          monthDiff = 11;
-        }
-        dayDiff += daysInMonth[startDate.getMonth()];
-      }
-    
-      return yearDiff + 'Y ' + monthDiff + 'M ' + dayDiff + 'D';
-    }
-    */
     
    useEffect(() => {
     setFromDate(fiveYearsAgo)
