e40876cd1d467aa462641d5c9e5d7204656aec06
Leggenn <eirik.andersen@nav.no>
2023-06-29 12:31:35 +0200

    Fått utregning av differanse til å funke

--- src/components/DateChooser.tsx
+++ src/components/DateChooser.tsx
@@ -36,7 +36,7 @@ const DateChooser = () => {
   } 
 
 
-  const { datepickerProps, toInputProps, fromInputProps, selectedRange } =
+  const { datepickerProps, toInputProps, fromInputProps} =
     useRangeDatepicker({
       fromDate: fromDate,
       toDate: toDate, 
