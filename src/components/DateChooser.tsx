771e64c3fb79a3981d49234baecda2ba92b5c9a8
Leggenn <eirik.andersen@nav.no>
2023-07-04 16:41:07 +0200

    Fått til clearing av form på 3/4 inputs.
    Co-authored-by: Stein Olav Løset Frey <594760@users.noreply.github.com>
    Co-authored-by: Jonas Nicolaysen <jonasnico@users.noreply.github.com>

--- src/components/DateChooser.tsx
+++ src/components/DateChooser.tsx
@@ -1,31 +1,42 @@
-import { RadioGroup, DatePicker, Radio, useRangeDatepicker, Select, Button, Heading} from "@navikt/ds-react";
-import { subYears, startOfYear, addYears, endOfYear } from 'date-fns';
+import {
+  RadioGroup,
+  DatePicker,
+  Radio,
+  useRangeDatepicker,
+  Select,
+  Button,
+  Heading,
+} from "@navikt/ds-react";
+import { subYears, startOfYear, addYears, endOfYear } from "date-fns";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import "./DateChooser.css"
+import "./DateChooser.css";
 import { Reise } from "../models/Reise";
 import Land from "./Land";
 
-const DateChooser = ({ data, setTableData } : { data: Array<Reise>, setTableData: Dispatch<SetStateAction<Array<Reise>>>}) => {
-  
+const DateChooser = ({
+  data,
+  setTableData,
+}: {
+  data: Array<Reise>;
+  setTableData: Dispatch<SetStateAction<Array<Reise>>>;
+}) => {
   const [initialStartDate, setInitialStartDate] = useState(new Date());
   const [initialEndDate, setInitialEndDate] = useState(new Date());
   const [differenceInDays, setDifferenceInDays] = useState<number>(0);
   const [land, setLand] = useState("");
   const [fromDate, setFromDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date())
+  const [toDate, setToDate] = useState(new Date());
   const [EØS, setEØS] = useState([]);
   const [formål, setFormål] = useState("");
-  
-
 
   const fiveYearsAgo = () => {
     const today = new Date();
     const fiveYearsAgo = subYears(today, 5);
-  
+
     const firstOfJanuary = startOfYear(fiveYearsAgo);
 
     return firstOfJanuary;
-  }
+  };
 
   const twoYearsForward = () => {
     const today = new Date();
@@ -33,40 +44,38 @@ const DateChooser = ({ data, setTableData } : { data: Array<Reise>, setTableData
 
     const lastOfDecember = endOfYear(twoForward);
     return lastOfDecember;
-  }
+  };
 
-  const daysBetweenDates = (from: Date, to:Date) : Number => {
-
+  const daysBetweenDates = (from: Date, to: Date): Number => {
     var diff = Math.abs(from.getTime() - to.getTime());
-    var diffDays = Math.ceil(diff/ (1000*3600*24));
+    var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
     console.log("Difference in days", diffDays);
     setDifferenceInDays(diffDays);
     return diffDays;
-  } 
+  };
 
+  const { datepickerProps, toInputProps, fromInputProps } = useRangeDatepicker({
+    fromDate: initialStartDate,
+    toDate: initialEndDate,
+    onRangeChange: (selectedRange) => {
+      if (
+        selectedRange?.from !== undefined &&
+        selectedRange?.to !== undefined
+      ) {
+        daysBetweenDates(selectedRange.from, selectedRange.to);
+        console.log(differenceInDays);
+        console.log(initialStartDate, initialEndDate);
+      }
+    },
+  });
 
-  const { datepickerProps, toInputProps, fromInputProps } =
-    useRangeDatepicker({
-      fromDate: initialStartDate,
-      toDate: initialEndDate, 
-      onRangeChange: (selectedRange) => {
-   
-        if (selectedRange?.from !== undefined && selectedRange?.to !== undefined) {
-          daysBetweenDates(selectedRange.from, selectedRange.to);
-          console.log(differenceInDays);
-          console.log(initialStartDate, initialEndDate)
-        }
-      }, 
-    });
-    
-   useEffect(() => {
-    setInitialStartDate(fiveYearsAgo)
-    setInitialEndDate(twoYearsForward)
-   }, [])  
+  useEffect(() => {
+    setInitialStartDate(fiveYearsAgo);
+    setInitialEndDate(twoYearsForward);
+  }, []);
 
-   // land, fraDato, tilDato, varighet, EØS, formål
-   function handleSubmit(event: any) {
-
+  // land, fraDato, tilDato, varighet, EØS, formål
+  function handleSubmit(event: any) {
     event.preventDefault();
 
     let nyReise: Reise = {
@@ -75,86 +84,96 @@ const DateChooser = ({ data, setTableData } : { data: Array<Reise>, setTableData
       tilDato: toDate,
       varighet: differenceInDays,
       EØS: EØS[0],
-      formål: formål
-    }
-      const copy = [...data];
-      copy.push(nyReise);
-      setTableData(copy);
+      formål: formål,
+    };
+    const copy = [...data];
+    copy.push(nyReise);
+    setTableData(copy);
 
-      resetInputFields();
+    resetInputFields();
+  }
 
-    }
+  const resetInputFields = () => {
+    setLand("");
+    setFromDate(new Date());
+    setToDate(new Date());
+    setDifferenceInDays(0);
+    setEØS([]);
+    setFormål("Ferie");
+  };
 
-    const resetInputFields = () => {
-      setLand("");
-      setFromDate(new Date());
-      setToDate(new Date());
-      setDifferenceInDays(0);
-      setEØS([]);
-      setFormål("");
-    }
+  const handleFromDateChange = (value: any) => {
+    setFromDate(value);
+  };
 
-    const handleFromDateChange = (event: any🦟) => {
-      event.preventDefault();
-      const target = event.target;
-      setFromDate(target.value);
-    }
+  const handleToDateChange = (value: any) => {
+    setToDate(value);
+  };
 
-    const handleToDateChange = (event: any) => {
-      event.preventDefault();
-      const target = event.target;
-      setToDate(target.value);
-    }
+  const handleEøsChange = (value: any) => {
+    setEØS(value);
+  };
 
-    const handleEøsChange = (event: any) => {
-      event.preventDefault();
-      const target = event.target;
-      setEØS(target.value);
-    }
-
-    const handleFormålChange = (event: any) => {
-      event.preventDefault();
-      const target = event.target;
-      setFormål(target.value);
-    }
-
-    
+  const handleFormålChange = (event: any) => {
+    const target = event.target;
+    setFormål(target.value);
+  };
 
   return (
     <div className="card">
       <Heading level="1" size="xlarge">
-      Feriekalkulator
-    </Heading>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Land setLand={setLand}/>
-      </div>
-      <DatePicker {...datepickerProps} dropdownCaption >
-        <div className="datepicker">
-          <DatePicker.Input id="fraDato" onChange={handleFromDateChange} {...fromInputProps} label="Fra"/>
-          <DatePicker.Input id="tilDato" onChange={handleToDateChange} {...toInputProps} label="Til"/>
+        Feriekalkulator
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Land land={land} setLand={setLand} />
         </div>
-      </DatePicker>
-      <div>Du har vært {differenceInDays.toString()} dager i utlandet</div>
+        <DatePicker {...datepickerProps} dropdownCaption>
+          <div className="datepicker">
+            <DatePicker.Input
+              id="fraDato"
+              onChange={handleFromDateChange}
+              {...fromInputProps}
+              label="Fra"
+            />
+            <DatePicker.Input
+              id="tilDato"
+              onChange={handleToDateChange}
+              {...toInputProps}
+              label="Til"
+            />
+          </div>
+        </DatePicker>
+        <div>Du har vært {differenceInDays.toString()} dager i utlandet</div>
 
-      <RadioGroup legend="Innenfor EØS?" id="EØS" onChange={handleEøsChange}>
-        <Radio value="Ja">Ja</Radio>
-        <Radio value="Nei">Nei</Radio>
-      </RadioGroup>
+        <RadioGroup
+          value={EØS}
+          onChange={handleEøsChange}
+          legend="Innenfor EØS?"
+          id="EØS"
+        >
+          <Radio value="Ja">Ja</Radio>
+          <Radio value="Nei">Nei</Radio>
+        </RadioGroup>
 
-      <Select className="dropdown" id="formål" label="Formål med reisen?" onChange={handleFormålChange}>
-        <option value="Ferie">Ferie</option>
-        <option value="Jobb">Jobb</option>
-        <option value="Annet">Annet</option>
-      </Select>
+        <Select
+          className="dropdown"
+          id="formål"
+          value={formål}
+          label="Formål med reisen?"
+          onChange={handleFormålChange}
+        >
+          <option value="Ferie">Ferie</option>
+          <option value="Jobb">Jobb</option>
+          <option value="Annet">Annet</option>
+        </Select>
 
-      <Button variant="primary" type="submit" >Legg til</Button>
+        <Button variant="primary" type="submit">
+          Legg til
+        </Button>
       </form>
-
     </div>
   );
-}
-
-
+};
 
 export default DateChooser;
--- src/components/Land.tsx
+++ src/components/Land.tsx
@@ -1,15 +1,27 @@
 import { Dropdown, Select } from "@navikt/ds-react";
 import { Dispatch, SetStateAction } from "react";
 
-const Land = ({setLand} : {setLand: Dispatch<SetStateAction<string>>}) => {
+const Land = ({
+  land,
+  setLand,
+}: {
+  land: string;
+  setLand: Dispatch<SetStateAction<string>>;
+}) => {
   const handleChange = (event: any) => {
     event.preventDefault();
-    
+
     setLand(event.target.value);
-  }
-  
+  };
+
   return (
-    <Select onChange={handleChange} className="dropdown" id="land" label="Hvilket land har du oppholdt deg i?">
+    <Select
+      onChange={handleChange}
+      className="dropdown"
+      value={land}
+      id="land"
+      label="Hvilket land har du oppholdt deg i?"
+    >
       <option value="">Velg land</option>
       <option value="Norge">Norge</option>
       <option value="Sverige">Sverige</option>
