fbc5d70c5f280daa9373d75818a9c66c7e21a755
Leggenn <eirik.andersen@nav.no>
2023-07-17 12:46:05 +0200

    Fix import
    Co-authored-by: Jonas Nicolaysen <70371917+jonasnico@users.noreply.github.com>
    Co-authored-by: Stein Olav Løset Frey <89256648+594760@users.noreply.github.com>

--- src/components/EditModal.tsx
+++ src/components/EditModal.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "@navikt/ds-react";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { EditTravel } from "./EditTravel";
 
 export const EditModal = ({
