95b0c21572e97db8f64d34a5029c899652512ac0
Stein Olav F <stein.olav.loset.frey@nav.no>
2023-07-19 17:19:44 +0200

    La til nav icon og endert navnet i taben
    Co-authored-by: Leggenn <eirik.andersen@nav.no>
    Co-authored-by: Jonas Nicolaysen <70371917+jonasnico@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import { useState } from "react";
 import JourneyTable from "@/components/JourneyTable";
 import { Travel } from "@/models/Travel";
 
-
 export default function Home() {
   const [savedTravels, setSavedTravels] = useState<Array<Travel>>([]);
   return (
     <div className="bg-gray-100 flex flex-wrap items-start gap-[var(--a-spacing-4)] mt-[var(--a-spacing-7)] ">
-        
-        <DateChooser
-          savedTravels={savedTravels}
-          setSavedTravels={setSavedTravels}
-        />
+      <title>Feriekalkulator</title>
+
+      <DateChooser
+        savedTravels={savedTravels}
+        setSavedTravels={setSavedTravels}
+      />
 
       <div className="overflow-auto max-h-[648px]">
         <JourneyTable
