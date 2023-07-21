0c06e7d97948637032d18e86e5b12658ebb98284
Stein Olav Løset Frey <89256648+594760@users.noreply.github.com>
2023-07-21 17:22:37 +0200

    Fix noe design (#8)
    Co-authored-by: Jonas Nicolaysen <70371917+jonasnico@users.noreply.github.com>

--- src/components/editAndDelete/EditModal.tsx
+++ src/components/editAndDelete/EditModal.tsx
@@ -2,6 +2,7 @@ import { Modal } from "@navikt/ds-react";
 import React, { Dispatch, SetStateAction, useEffect } from "react";
 import { EditTravel } from "./EditTravel";
 import { Travel } from "../../models/Travel";
+import { DocPencilIcon} from "@navikt/aksel-icons";
 
 export const EditModal = ({
   open,
@@ -25,13 +26,19 @@ export const EditModal = ({
 
   return (
     <Modal
-      className="vw-1/3 vh-2/3 flex px-20 py-12 gap-5 overflow-visible"
+      className="vw-1/3 vh-2/3 overflow-visible"
       open={open}
       aria-label="Rediger"
       onClose={() => setOpen((x) => !x)}
       closeButton={false}
       aria-labelledby="modal-heading"
     >
+      <div id="icon-container" className="top-8 justify-center relative flex">
+        <DocPencilIcon
+          title="a11y-title"
+          className="bg-orange-200 rounded-full w-16 h-16 p-1 top-0 "
+        />
+      </div>
       <EditTravel
         savedTravels={savedTravels}
         travelToEdit={travelToEdit}
