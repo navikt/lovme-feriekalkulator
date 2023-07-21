9c0f0975c8252840d41cae547fa80c205104cdb2
Stein Olav F <stein.olav.loset.frey@nav.no>
2023-07-21 16:16:22 +0200

    Endret språk på date-fns

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { setDefaultOptions } from "date-fns";
+import { nb } from "date-fns/locale";
+import { Html, Head, Main, NextScript } from "next/document";
+
+setDefaultOptions({ locale: nb });
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-gray-100 ml-5'>
+      <body className="bg-gray-100 ml-5">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
