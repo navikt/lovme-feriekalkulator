82959034692228e3b2f0d16255f01e4c58f81767
jonasnico <jonas.nicolaysen@nav.no>
2023-07-24 13:11:27 +0200

    fix navbar import
    Co-authored-by: Leggenn <eirik.andersen@nav.no>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
