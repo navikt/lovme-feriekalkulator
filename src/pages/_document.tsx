960cb3011457c0fa13a8c20c8213b564069f1d98
mvbraathen <marius.brathen@me.com>
2023-10-04 17:00:56 +0200

    Added decorator client side

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -7,7 +6,6 @@ export default function Document() {
       <Head />
 
       <body className="bg-gray-100">
-        <Navbar></Navbar>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { TravelForm } from "@/components/TravelForm";
 import { VisualTimeline } from "@/components/VisualTimeline";
 import { useEffect, useState } from "react";
+import { injectDecoratorClientSide } from "@navikt/nav-dekoratoren-moduler";
 import JourneyTable from "@/components/JourneyTable";
 import { Travel } from "@/models/Travel";
 import SummaryCard from "@/components/SummaryCard";
@@ -8,6 +9,15 @@ import { setDefaultOptions } from "date-fns";
 import { nb } from "date-fns/locale";
 import { getAllRedTravels } from "@/utilities/ruleEngine";
 setDefaultOptions({ locale: nb });
+
+injectDecoratorClientSide({
+  env: "dev",
+  params: {
+    simple: true,
+    chatbot: false,
+    shareScreen: false,
+  },
+});
 
 export default function Home() {
   const [savedTravels, setSavedTravels] = useState<Array<Travel>>([]);
